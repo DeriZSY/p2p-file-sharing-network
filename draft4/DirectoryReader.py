be4ea0e46246521fe155d66a58cfab7777122f3b
Beau Carlborg <bcarlborg@gmail.com>
2018-10-15 22:36:52 -0600

    more stubbed info

---
+++ draft4/DirectoryReader.py
@@ -0,0 +1,6 @@
+class DirectoryReader():
+    def __init__(self, shared_dir):
+        self.shared_dir = shared_dir
+
+    # Function returns a list of file name strings
+    def list_file_names(self):
